d634668ec3cc31390eac26cf7411230bb5a25739
Peter Ehrlich <peter.i.ehrlich@gmail.com>
2013-10-14 14:10:33 -0700

    Bring Sample.java up to date with leap API
    These changes allow it to compile once more

--- Sample.java
+++ Sample.java
@@ -1,4 +1,17 @@
+package leap;
+
+//import java.lang.System;
+
 import java.io.IOException;
+
+import com.leapmotion.leap.Controller;
+import com.leapmotion.leap.Finger;
+import com.leapmotion.leap.FingerList;
+import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Hand;
+import com.leapmotion.leap.HandList;
+import com.leapmotion.leap.Listener;
+import com.leapmotion.leap.Vector;
 
 /******************************************************************************\
 * Copyright (C) 2012 Leap Motion, Inc. All rights reserved.                    *
@@ -8,7 +21,7 @@ import java.io.IOException;
 * This software is subject to copyright.                                       *
 \******************************************************************************/
 
-class SampleListener extends Listener {
+class LeapListener extends Listener {
   public void onInit(Controller controller) {
     System.out.println("Initialized");
   }
@@ -24,8 +37,8 @@ class SampleListener extends Listener {
   public void onFrame(Controller controller) {
     // Get the most recent frame and report some basic information
     Frame frame = controller.frame();
-    HandArray hands = frame.hands();
-    long numHands = hands.size();
+    HandList hands = frame.hands();
+    long numHands = hands.count();
     System.out.println("Frame id: " + frame.id()
                      + ", timestamp: " + frame.timestamp()
                      + ", hands: " + numHands);
@@ -35,17 +48,17 @@ class SampleListener extends Listener {
       Hand hand = hands.get(0);
 
       // Check if the hand has any fingers
-      FingerArray fingers = hand.fingers();
-      long numFingers = fingers.size();
+      FingerList fingers = hand.fingers();
+      long numFingers = fingers.count();
       if (numFingers >= 1) {
         // Calculate the hand's average finger tip position
         Vector pos = new Vector(0, 0, 0);
         for (int i = 0; i < numFingers; ++i) {
           Finger finger = fingers.get(i);
-          Ray tip = finger.tip();
-          pos.setX(pos.getX() + tip.getPosition().getX());
-          pos.setY(pos.getY() + tip.getPosition().getY());
-          pos.setZ(pos.getZ() + tip.getPosition().getZ());
+          Vector tipPosition = finger.stabilizedTipPosition();
+          pos.setX(pos.getX() + tipPosition.getX());
+          pos.setY(pos.getY() + tipPosition.getY());
+          pos.setZ(pos.getZ() + tipPosition.getZ());
         }
         pos = new Vector(pos.getX()/numFingers, pos.getY()/numFingers, pos.getZ()/numFingers);
         System.out.println("Hand has " + numFingers + " fingers with average tip position"
@@ -53,11 +66,10 @@ class SampleListener extends Listener {
       }
 
       // Check if the hand has a palm
-      Ray palmRay = hand.palm();
-      if (palmRay != null) {
+      if (hand.isValid()) {
         // Get the palm position and wrist direction
-        Vector palm = palmRay.getPosition();
-        Vector wrist = palmRay.getDirection();
+        Vector palm = hand.palmPosition();
+        Vector wrist = hand.palmNormal();
         String direction = "";
         if (wrist.getX() > 0)
           direction = "left";
@@ -70,10 +82,10 @@ class SampleListener extends Listener {
   }
 }
 
-class Sample {
+class Leap {
   public static void main(String[] args) {
     // Create a sample listener and assign it to a controller to receive events
-    SampleListener listener = new SampleListener();
+    LeapListener listener = new LeapListener();
     Controller controller = new Controller(listener);
 
     // Keep this process running until Enter is pressed
@@ -81,10 +93,10 @@ class Sample {
     try {
 		System.in.read();
 	} catch (IOException e) {
-		
+
 	}
 
     //The controller must be disposed of before the listener
-    controller = null;
+//    controller = null;
   }
 }
